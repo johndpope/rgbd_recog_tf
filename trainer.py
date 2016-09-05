8a8c717fe65a955f8fac74f4688860396d0bbae9
Khoi-Nguyen MAC <knmac@wirelessprv-10-194-136-37.near.illinois.edu>
2016-09-05 12:01:44 -0500

    big change

--- trainer.py
+++ trainer.py
@@ -9,52 +9,81 @@ DIR_CKPT    = 'checkpoints'
 DIR_LST     = 'lists'
 DIR_MODEL   = 'models'
 DIR_SUMMARY = 'summary'
+DIR_DATA    = os.path.join('/','home','knmac','washington','fullset','cropped')
 
-LST_TRAIN   = os.path.join(DIR_LST, '')
-LST_EVAL    = os.path.join(DIR_LST, '')
+
+LST_TRAIN   = os.path.join(DIR_LST, 'train_full.lst')
+LST_EVAL    = os.path.join(DIR_LST, 'eval_full.lst')
+LST_DICT    = os.path.join(DIR_LST, 'dictionary_full.lst')
 WEIGHT_ALEX = os.path.join(DIR_MODEL, 'bvlc_alexnet.npy')
 
+CLASSES = open(LST_DICT, 'r').read().splitlines()
 
 # basic model parameters
 FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_integer('batch_size', 128, """Numer of images to process in a batch.""")
+tf.app.flags.DEFINE_integer('max_iter', 1000, """Maximum number of training iteration.""")
+tf.app.flags.DEFINE_integer('batch_size', 200, """Numer of images to process in a batch.""")
 tf.app.flags.DEFINE_integer('img_s', 227, """"Size of a square image.""")
+tf.app.flags.DEFINE_integer('n_classes', 51, """Number of classes.""")
 tf.app.flags.DEFINE_float('learning_rate', 1e-4, """"Learning rate for training models.""")
+
 
 #=========================================================================================
 def placeholder_inputs(batch_size=None):
     images_ph = tf.placeholder(tf.float32, shape=(batch_size, FLAGS.img_s, FLAGS.img_s, 3)) #TODO:check dimension's order
-    labels_ph = tf.placeholder(tf.int32, shape=(batch_size, 1000))
+    labels_ph = tf.placeholder(tf.int32, shape=(batch_size, FLAGS.n_classes))
 
     return images_ph, labels_ph
 
 
-def fill_feed_dict():
+def fill_feed_dict(train_idx, batch_idx):
     """Fills the feed_dict for training the given step
     """
-    return
+    N = train_idx.shape[0]
+
+    if batch_id + FLAGS.batch_size > N:
+        feed_dict = {}
+        batch_idx = -1
+    else:
+        feed_dict = {}
+        batch_idx += FLAGS.batch_size
+    return feed_dict, batch_idx
 
 
 #=========================================================================================
 def run_training():
+    net_data = np.load(WEIGHT_ALEX).item()
+
+    # tensorflow variables and operations
     images_ph, labels_ph = placeholder_inputs()
 
-    net_data = np.load(WEIGHT_ALEX).item()
     prob = model1.inference(images_ph, net_data, is_training=True)
     loss = model1.loss(prob, labels_ph)
     train_op = model1.training(loss)
     eval_correct = model1.evaluation(prob, labels_ph)
-
-
-    summary = tf.merge_all_summaries()
     init_op = tf.initialize_all_variables()
-    saver = tf.train.Saver()
 
+    # tensorflow monitor
+    summary = tf.merge_all_summaries()
+    saver = tf.train.Saver()
+   
+    # initialize graph
     sess = tf.Session()
     summary_writer = tf.train.SummaryWriter(DIR_SUMMARY, sess.graph)
-
-
     sess.run(init_op)
+
+
+    # start the training loop
+    for step in range(FLAGS.max_iter):
+        # training phase
+        rgb_paths, dep_paths, labels = common.get_paths_labels(DIR_DATA, LST_TRAIN, CLASSES, to_shuffle=True)
+
+        batch_idx = 0
+        while batch_idx != -1:
+            fd, batch_idx = fill_feed_dict(train_idx, batch_idx)
+
+        # evaluation phase
+        rgb_paths, dep_paths, labels = common.get_paths_labels(DIR_DATA, LST_EVAL, CLASSES, to_shuffle=False)
     return
 
 
--- utils/common.py
+++ utils/common.py
@@ -1,2 +1,75 @@
+import os, sys, glob, ipdb
+import numpy as np
+
+
 def early_stopping():
+    return
+
+
+def get_sample_ids(dir_data, pth_lst):
+    """Generate the paths to samples of a list
+    Args:
+        dir_data: where the data are stored
+        pth_lst: list containing objects' names, e.g. training list, evaluation list, testing list
+    Returns:
+        sample_ids: paths of all sample ID that belong to the objects in pth_lst
+    """
+
+    def _get_sample_ids_of_obj(pth):
+        """Get the indices (without extension) of samples in a directory.
+        Args:
+            pth: directory to search.
+        Returns:
+            idx_lst: list of indices without extensions
+        """
+        assert os.path.exists(pth), pth+' does not exist.'
+
+        pwd = os.getcwd()
+        os.chdir(pth)
+        idx_lst = glob.glob('*_depth.png')
+        idx_lst = ' '.join(idx_lst).replace('_depth.png', '').split()
+        idx_lst.sort()
+        os.chdir(pwd)
+        return idx_lst
+
+    objects = open(pth_lst, 'r').read().splitlines()
+    sample_ids = []
+    for obj in objects:
+        pth = os.path.join(dir_data, obj)
+        sample_ids +=  _get_sample_ids_of_obj(pth)
+    return sample_ids
+
+
+def get_paths_labels(dir_data, pth_lst, classes, to_shuffle):
+    """Get the full paths and labels of samples
+    Args:
+        dir_data: directory containing data
+        pth_lst: path to list
+        classes: array of all possible classes
+        to_shuffle: samples should be shuffled (for training) or not
+    Returns:
+        rgb_paths: paths to all rgb images
+        dep_paths: paths to all depth images
+        labels: list of labels, each row is a binary array, where 1 means the class the sample belongs to
+    """
+    sample_ids = get_sample_ids(dir_data, pth_lst)
+    if to_shuffle:
+        np.random.shuffle(sample_ids)
+
+    rgb_paths = ['']*len(sample_ids)
+    dep_paths = ['']*len(sample_ids)
+    labels = np.zeros((len(sample_ids), len(classes)))
+    for i in range(len(sample_ids)):
+        tokens = sample_ids[i].split('_')
+        L = len(tokens)
+        class_id = '_'.join(tokens[:L-3])
+        obj_id   = '_'.join(tokens[:L-2])
+
+        rgb_paths[i] = os.path.join(dir_data, class_id, obj_id, sample_ids[i]+'.png')
+        dep_paths[i] = os.path.join(dir_data, class_id, obj_id, sample_ids[i]+'_depth.png')
+        labels[i, classes.index(class_id)] = 1
+    return rgb_paths, dep_paths, labels
+
+
+def load_images():
     return
