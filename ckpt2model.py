d1bcdc10d70f76ac88a2c0b4d0125f62fa27c7fd
Khoi-Nguyen MAC <knmac@wirelessprv-10-194-136-37.near.illinois.edu>
2016-10-04 17:13:21 -0500

    Add model converter

--- ckpt2model.py
+++ ckpt2model.py
@@ -3,38 +3,95 @@ import tensorflow as tf
 import configure as cfg
 import os, ipdb
 
+RGB_CKPT_ID = '6'
+DEP_CKPT_ID = '12'
+
+def load_tf_model(tag):
+    tag = tag + '_'
+
+    var_lst = []
+    # conv-1 layer
+    with tf.name_scope(tag+'conv1') as scope:
+        conv1W = tf.Variable(tf.zeros(shape=[11,11,3,96]), name='weight')
+        conv1b = tf.Variable(tf.zeros(shape=[96]), name='biases')
+
+    # conv-2 layer
+    with tf.name_scope(tag+'conv2') as scope:
+        conv2W = tf.Variable(tf.zeros(shape=[5,5,48,256]), name='weight')
+        conv2b = tf.Variable(tf.zeros(shape=[256]), name='biases')
+
+    # conv-3 layer
+    with tf.name_scope(tag+'conv2') as scope:
+        conv3W = tf.Variable(tf.zeros(shape=[3,3,256,384]), name='weight')
+        conv3b = tf.Variable(tf.zeros(shape=[384]), name='biases')
+
+    # conv-4 layer
+    with tf.name_scope(tag+'conv4') as scope:
+        conv4W = tf.Variable(tf.zeros(shape=[3,3,192,384]), name='weight')
+        conv4b = tf.Variable(tf.zeros(shape=[384]), name='biases')
+
+    # conv-5 layer
+    with tf.name_scope(tag+'conv5') as scope:
+        conv5W = tf.Variable(tf.zeros(shape=[3,3,192,256]), name='weight')
+        conv5b = tf.Variable(tf.zeros(shape=[256]), name='biases')
+
+    # fc6 layer
+    with tf.name_scope(tag+'fc6') as scope:
+        fc6W = tf.Variable(tf.zeros(shape=[9216,4096]), name='weight')
+        fc6b = tf.Variable(tf.zeros(shape=[4096]), name='biases')
+
+    # fc7 layer
+    with tf.name_scope(tag+'fc7') as scope:
+        fc7W = tf.Variable(tf.zeros(shape=[4096,4096]), name='weight')
+        fc7b = tf.Variable(tf.zeros(shape=[4096]), name='biases')
+
+    # fc8 layer - classifier
+    with tf.name_scope(tag+'fc8') as scope:
+        fc8W = tf.Variable(tf.zeros([4096,51]), name='weight')
+        fc8b = tf.Variable(tf.zeros([51]), name='biases')
+
+    var_lst = [conv1W, conv1b, conv2W, conv2b, conv3W, conv3b, conv4W, conv4b, conv5W, conv5b, fc6W, fc6b, fc7W, fc7b, fc8W, fc8b]
+    return var_lst
 
 
-def main():
-    #net_data = np.load(cfg.PTH_WEIGHT_ALEX).item()
-
-
-    net_data = np.load(cfg.PTH_WEIGHT_ALEX).item()
-    with tf.variable_scope('rgb'):
-        with tf.name_scope('conv1'):
-            conv1_rgb = tf.Variable(tf.zeros(net_data['conv1'][0].shape, name='weight'))
-    
-    with tf.variable_scope('dep'):
-        with tf.name_scope('conv1'):
-            conv1_dep = tf.Variable(tf.zeros(net_data['conv1'][0].shape, name='weight'))
-    '''
-    with tf.variable_scope('rgb'):
-        with tf.name_scope('conv1'):
-            conv1_rgb = tf.Variable(tf.zeros([11,11,3,96]), name='weight')  # dummy variable !!!
-    '''
-
+def convert(tag, path):
+    var_lst = load_tf_model(tag)
     saver = tf.train.Saver()
     sess = tf.Session()
     init_op = tf.initialize_all_variables()
     sess.run(init_op)
+    saver.restore(sess, path)
 
+    model = dict()
+    tag += '_'
+    model[tag+'conv1W'] = sess.run(var_lst[0])
+    model[tag+'conv1b'] = sess.run(var_lst[1])
+    model[tag+'conv2W'] = sess.run(var_lst[2])
+    model[tag+'conv2b'] = sess.run(var_lst[3])
+    model[tag+'conv3W'] = sess.run(var_lst[4])
+    model[tag+'conv3b'] = sess.run(var_lst[5])
+    model[tag+'conv4W'] = sess.run(var_lst[6])
+    model[tag+'conv4b'] = sess.run(var_lst[7])
+    model[tag+'conv5W'] = sess.run(var_lst[8])
+    model[tag+'conv5b'] = sess.run(var_lst[9])
+    model[tag+'fc6W'] = sess.run(var_lst[10])
+    model[tag+'fc6b'] = sess.run(var_lst[11])
+    model[tag+'fc7W'] = sess.run(var_lst[12])
+    model[tag+'fc7b'] = sess.run(var_lst[13])
+    model[tag+'fc8W'] = sess.run(var_lst[14])
+    model[tag+'fc8b'] = sess.run(var_lst[15])
 
-    ipdb.set_trace()
-    saver.restore(sess, 'checkpoints/dep-10')
+    np.save(os.path.join(cfg.DIR_MODEL, tag+'model.npy'), model)
     return
 
 
 
 if __name__ == '__main__':
+    print 'Converting RGB model...'
     with tf.Graph().as_default():
-        main()
+        convert('rgb', os.path.join(cfg.DIR_CKPT, 'rgb-'+RGB_CKPT_ID))
+
+    print 'Converting Dep model...'
+    with tf.Graph().as_default():
+        convert('dep', os.path.join(cfg.DIR_CKPT, 'dep-'+DEP_CKPT_ID))
+
