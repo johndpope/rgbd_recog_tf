052ade14cdf8dd2a658868cc1114e5c462dedd21
Khoi-Nguyen MAC <knmac@wirelessprv-10-194-136-37.near.illinois.edu>
2016-11-22 13:35:01 -0600

    remove files with missing masks

---
+++ remove_missing_masks.sh
@@ -0,0 +1,3 @@
+mv ~/data/rgbd-dataset/binder/binder_3/binder_3_1_236* ~/data/
+mv ~/data/rgbd-dataset/binder/binder_3/binder_3_1_237* ~/data/
+mv ~/data/rgbd-dataset/binder/binder_3/binder_3_1_238* ~/data/
--- replicate_missing_masks.sh
+++
@@ -1,3 +0,0 @@
-cp ~/data/rgbd-dataset/binder/binder_3/binder_3_1_235_maskcrop.png ~/data/rgbd-dataset/binder/binder_3/binder_3_1_236_maskcrop.png
-cp ~/data/rgbd-dataset/binder/binder_3/binder_3_1_235_maskcrop.png ~/data/rgbd-dataset/binder/binder_3/binder_3_1_237_maskcrop.png
-cp ~/data/rgbd-dataset/binder/binder_3/binder_3_1_235_maskcrop.png ~/data/rgbd-dataset/binder/binder_3/binder_3_1_238_maskcrop.png
