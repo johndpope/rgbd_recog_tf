a06572fac3d89bb7ae02e0355adb5693b0279e2b
knmac <khoinguyenx@gmail.com>
2016-12-03 10:08:26 -0600

    add features for visualizer

--- visualize.py
+++ visualize.py
@@ -5,7 +5,7 @@ import os, ipdb
 
 
 EXPERIMENT = 'rgbeval_9'
-N_SAMPLES = 150
+N_SAMPLES = 100
 
 if __name__ == '__main__':
     pth = os.path.join(cfg.DIR_PROB, EXPERIMENT+'.txt')
@@ -14,16 +14,25 @@ if __name__ == '__main__':
     prob = data[:,:51]
     predict = data[:,51]
     lbl = data[:,52].astype(np.int32)
-    truth = np.zeros((N,51));
-    for i in range(N): truth[i,lbl[i]] = 1
+
+    truth = np.zeros((N,51))
+    for i in range(N): 
+        truth[i,lbl[i]] = 1
 
     plt.figure()
     ids = np.random.choice(N, N_SAMPLES, replace=False)
-
     plt.subplot(1,2,1)
     plt.imshow(prob[ids])
     plt.colorbar()
-
+    plt.title('probability')
     plt.subplot(1,2,2)
     plt.imshow(truth[ids])
+    plt.title('groundtruth')
+
+    plt.figure()
+    plt.plot(predict[ids], 'b-', label='prediction')
+    plt.plot(lbl[ids], 'r-', label='truth')
+    plt.legend()
+    plt.title('top-1 prediction + groundtruth')
+
     plt.show()
