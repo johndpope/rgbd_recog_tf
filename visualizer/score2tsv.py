84ed2ce0449268182d3695af9038844ecae61978
knmac <khoinguyenx@gmail.com>
2016-12-18 15:14:46 -0600

    fix score2tsv

--- visualizer/score2tsv.py
+++ visualizer/score2tsv.py
@@ -1,16 +1,18 @@
 import numpy as np
-import ipdb
+import sys, ipdb
 
-score = np.loadtxt('./rgbeval_49.txt')
-lst = open('./eval_2.lst').read().splitlines()
+lst = open('../lists/eval_1.lst').read().splitlines() #TODO
 
-score = score[:,:51]
-np.savetxt('data.tsv', score, delimiter='\t', fmt='%.4f')
+if __name__ == '__main__':
+    score_path = sys.argv[1]
+    score = np.loadtxt(score_path)
+    score = score[:,:51]
+    np.savetxt('data.tsv', score, delimiter='\t', fmt='%.4f')
 
-f = open('metadata.tsv', 'w')
-f.write('Sample\tCategory\n')
-for i in lst:
-    toks = i.split('/')
-    f.write('%s\t%s\n' % (toks[2], toks[0]))
-f.close()
+    f = open('metadata.tsv', 'w')
+    f.write('Category\tObject\tSample\n')
+    for i in lst:
+        toks = i.split('/')
+        f.write('%s\t%s\n' % (toks[0], toks[1], toks[2]))
+    f.close()
 
